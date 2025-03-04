d7145305b876532d594237f568c90a9fa6c31171
Jamshaa <125591505+Jamshaa@users.noreply.github.com>
2025-03-04 18:37:06 +0100

    Add files via upload

---
+++ cooking/RecipeSearch/RecipeSearch/settings.py
@@ -0,0 +1,83 @@
+import os
+from pathlib import Path
+
+# Directorio base del proyecto
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'tu-clave-secreta-aqui'
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Aplicaciones instaladas
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'recipes',  # Nuestra app
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'RecipeSearch.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "recipes" / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'RecipeSearch.wsgi.application'
+
+# Base de datos (SQLite por defecto)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
+
+# Validadores de contraseñas
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    # Puedes agregar más validadores si lo deseas
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Archivos estáticos
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "recipes" / "static"]
+
+# Configuración de la API de Spoonacular
+SPOONACULAR_API_KEY = '366df8cb3a0f4e65bd80ab09919434f4'
+SPOONACULAR_SEARCH_URL = 'https://api.spoonacular.com/recipes/complexSearch'
