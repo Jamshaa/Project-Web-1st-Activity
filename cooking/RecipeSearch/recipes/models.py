5241c3990187031ea9d96ebc56a4f734265dd8c3
Jamshaa <125591505+Jamshaa@users.noreply.github.com>
2025-03-05 15:37:25 +0100

    Add files via upload

---
+++ cooking/RecipeSearch/recipes/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)  # Category name
+    description = models.TextField(blank=True)           # Optional description
+
+    def __str__(self):
+        return self.name
+
+class Recipe(models.Model):
+    spoonacular_id = models.IntegerField(unique=True)
+    title = models.CharField(max_length=100)
+    image = models.URLField(blank=True)
+    instructions = models.TextField(blank=True)
+    # Link to category; can be null if not assigned
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    def __str__(self):
+        return f'{self.user.username} Profile'
